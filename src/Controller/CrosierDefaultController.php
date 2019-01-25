ec861d00b92a961002245b41a79d4df29df0d046
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-01-25 12:01:21 -0200

    MainMenu

---
+++ src/Controller/CrosierDefaultController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlos
+ * Date: 25/01/19
+ * Time: 11:17
+ */
+
+namespace CrosierSource\CrosierLibBaseBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Interface CrosierDefaultController.
+ *
+ * Todos os CrosierApps devem ter um DefaultController implementando esta interface.
+ *
+ * @package CrosierSource\CrosierLibBaseBundle\Controller
+ * @author Carlos Eduardo Pauluk
+ */
+abstract class CrosierDefaultController extends AbstractController
+{
+
+    /**
+     * @return mixed
+     */
+    public abstract function index();
+
+    /**
+     *
+     * @return mixed
+     */
+    public abstract function getAppMainMenu();
+
+}
