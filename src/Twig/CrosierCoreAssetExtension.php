cff9dc8799fa2b8f38ea8f7bf3167f073455b2ee
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-02-19 17:23:30 -0300

    Logger para src/Twig/CrosierCoreAssetExtension.php

--- src/Twig/CrosierCoreAssetExtension.php
+++ src/Twig/CrosierCoreAssetExtension.php
@@ -5,13 +5,20 @@ namespace CrosierSource\CrosierLibBaseBundle\Twig;
 use CrosierSource\CrosierLibBaseBundle\Exception\ViewException;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Psr\Log\LoggerInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 
 class CrosierCoreAssetExtension extends AbstractExtension
 {
+    /** @var LoggerInterface */
+    private $logger;
 
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     public function getFunctions()
     {
@@ -27,9 +34,11 @@ class CrosierCoreAssetExtension extends AbstractExtension
      */
     function getCrosierAsset($asset)
     {
-
         try {
+            $this->logger->info(str_repeat('.',100));
+            $this->logger->info('getCrosierAsset(' . $asset . ')');
             $base_uri = getenv('CROSIERCORE_URL');
+            $this->logger->info($base_uri);
             $client = new Client(['base_uri' => $base_uri]);
             $uri = $base_uri . '/getCrosierAssetUrl?asset=' . urlencode($asset);
             $response = $client->request('GET', $uri);
@@ -38,7 +47,9 @@ class CrosierCoreAssetExtension extends AbstractExtension
             $decoded = json_decode($jsonResponse, true);
             return $base_uri . $decoded['url'];
         } catch (GuzzleException $e) {
-            throw new ViewException('CrosierCoreAssetExtension:getCrosierAsset error');
+            $this->logger->error($e->getMessage());
+            return 'NOTFOUND/' . $asset;
+            // throw new ViewException('CrosierCoreAssetExtension:getCrosierAsset error');
         }
 
     }
