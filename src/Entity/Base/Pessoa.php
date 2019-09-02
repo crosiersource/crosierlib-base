1ac07eaa162eff8d1f079af707c9d5bfc86b132e
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2019-09-02 15:43:47 -0300

    ...

--- src/Entity/Base/Pessoa.php
+++ src/Entity/Base/Pessoa.php
@@ -210,6 +210,20 @@ class Pessoa implements EntityId
      * @return string
      * @Groups("entity")
      */
+    public function getNomeMontado(): string
+    {
+        if ($this->nomeFantasia) {
+            $r .= $this->nomeFantasia . ' (' . $this->nome . ')';
+        } else {
+            $r .= $this->nome;
+        }
+        return $r;
+    }
+
+    /**
+     * @return string
+     * @Groups("entity")
+     */
     public function getNomeMontadoComDocumento(): string
     {
         $r = $this->getDocumentoMascarado() . ' - ';
