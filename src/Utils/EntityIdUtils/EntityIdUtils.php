b72d464e772e74878019c7c286efbdfa4eb992a2
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-03-16 16:39:22 -0300

    Alterações no EntityIdUtils.php

--- src/Utils/EntityIdUtils/EntityIdUtils.php
+++ src/Utils/EntityIdUtils/EntityIdUtils.php
@@ -6,7 +6,6 @@ use CrosierSource\CrosierLibBaseBundle\Entity\EntityId;
 use CrosierSource\CrosierLibBaseBundle\Normalizer\EntityNormalizer;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
@@ -54,13 +53,15 @@ class EntityIdUtils
 
     /**
      * @param array $entities
+     * @param int $circularReferenceLimit
+     * @param array $groups
      * @return array
      */
-    public function serializeAll(array $entities): array
+    public function serializeAll(array $entities, int $circularReferenceLimit = 3, array $groups = ['entity', 'entityId']): array
     {
         $a = [];
         foreach ($entities as $e) {
-            $a[] = $this->serialize($e);
+            $a[] = $this->serialize($e, $circularReferenceLimit, $groups);
         }
         return $a;
     }
@@ -69,9 +70,11 @@ class EntityIdUtils
      * Serializa uma entidade para um json.
      *
      * @param EntityId $entityId
+     * @param int $circularReferenceLimit
+     * @param array $groups
      * @return array
      */
-    public static function serialize(EntityId $entityId): array
+    public static function serialize(EntityId $entityId, int $circularReferenceLimit = 3, array $groups = ['entity', 'entityId']): array
     {
         try {
             $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
@@ -79,8 +82,8 @@ class EntityIdUtils
             $serializer = new Serializer([new DateTimeNormalizer(), $normalizer, new ArrayDenormalizer()]);
             return $serializer->normalize($entityId, 'json',
                 [
-                    'groups' => ['entity', 'entityId'],
-                    'circular_reference_limit' => 3,
+                    'groups' => $groups,
+                    'circular_reference_limit' => $circularReferenceLimit,
                     'enable_max_depth' => true
                 ]);
         } catch (\Throwable $e) {
@@ -95,7 +98,7 @@ class EntityIdUtils
      * @param string $type
      * @return object
      */
-    public function unserialize(array $entityArray, string $type)
+    public function unserialize(array $entityArray, string $type, int $circularReferenceLimit = 3, array $groups = ['entity', 'entityId'])
     {
         try {
             $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
@@ -105,8 +108,8 @@ class EntityIdUtils
             return $serializer->denormalize($entityArray, $type, 'json',
                 [
                     ObjectNormalizer::DISABLE_TYPE_ENFORCEMENT => true,
-                    'groups' => ['entity', 'entityId'],
-                    'circular_reference_limit' => 3,
+                    'groups' => $groups,
+                    'circular_reference_limit' => $circularReferenceLimit,
                     'enable_max_depth' => true
                 ]);
         } catch (\Throwable $e) {
