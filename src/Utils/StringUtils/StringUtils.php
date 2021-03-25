cb8bc659201a14496db82452760ae728d665a32e
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-03-25 10:46:54 -0300

    TestsUtils... generators

--- src/Utils/StringUtils/StringUtils.php
+++ src/Utils/StringUtils/StringUtils.php
@@ -168,7 +168,7 @@ class StringUtils
         return (json_last_error() == JSON_ERROR_NONE);
     }
 
-    
+
     public static function strposRegex(string $subject, string $pattern)
     {
         preg_match('/' . $pattern . '/', $subject, $matches, PREG_OFFSET_CAPTURE);
@@ -179,14 +179,10 @@ class StringUtils
     public static function formataTelefone(string $numero)
     {
         $numero = preg_replace("/[^0-9]/", "", $numero);
-        if (strlen($numero) == 10) {
-            $numero = substr_replace($numero, '(', 0, 0);
-            $numero = substr_replace($numero, '9', 3, 0);
-            $numero = substr_replace($numero, ')', 3, 0);
-        } elseif (strlen($numero) == 11) {
-            $numero = substr_replace($numero, '(', 0, 0);
-            $numero = substr_replace($numero, ')', 3, 0);
-        }
+        $arrNumeros = str_split($numero);
+        $numero = (strlen($numero) == 11) ?
+            vsprintf('(%d%d) %d%d%d%d%d-%d%d%d%d', $arrNumeros) :
+            vsprintf('(%d%d) %d%d%d%d-%d%d%d%d', $arrNumeros);
         return $numero;
     }
 
--- src/Utils/TestsUtils/GeneratorsUtils.php
+++ src/Utils/TestsUtils/GeneratorsUtils.php
@@ -23,9 +23,9 @@ class GeneratorsUtils
             'Abreu', 'Adães', 'Adorno', 'Aguiar', 'Albuquerque', 'Alcântara', 'Aleluia', 'Alencar', 'Almeida', 'Altamirano', 'Alvarenga', 'Álvares', 'Alves', 'Alvim', 'Amaral', 'Amigo', 'Amor', 'Amorim', 'Anchieta', 'Andrada', 'Andrade', 'Anes', 'Anjos', 'Antunes', 'Anunciação', 'Aragão', 'Araújo', 'Arruda', 'Ascensão', 'Assis', 'Azeredo', 'Azevedo', 'Bandeira', 'Barbosa', 'Barros', 'Barroso', 'Bastos', 'Batista', 'Bermudes', 'Bernades', 'Bernardes', 'Bicalho', 'Bispo', 'Bocaiuva', 'Bolsonaro', 'Borba', 'Borges', 'Borsoi', 'Botelho', 'Braga', 'Bragança', 'Brandão', 'Brasil', 'Brasiliense', 'Bueno', 'Cabral', 'Café', 'Camacho', 'Camargo', 'Caminha', 'Camões', 'Cardoso', 'Carmo', 'Carnaval', 'Carneiro', 'Carvalhal', 'Carvalho', 'Carvalhosa', 'Castilho', 'Castro', 'Cerejeira', 'Chaves', 'Coelho', 'Coentrão', 'Coimbra', 'Constante', 'Cordeiro', 'Costa', 'Cotrim', 'Couto', 'Coutinho', 'Cruz', 'Cunha', 'Curado', 'Dambros', 'Dias', 'Diegues', 'Dorneles', 'Duarte', 'Eça', 'Encarnação', 'Esteves', 'Evangelista', 'Exaltação', 'Fagundes', 'Faleiros', 'Falópio', 'Falqueto', 'Faria', 'Farias', 'Faro', 'Ferrão', 'Ferraz', 'Ferreira', 'Ferrolho', 'Fernandes', 'Figo', 'Figueira', 'Figueiredo', 'Figueiroa', 'Fioravante', 'Fonseca', 'Fontes', 'Fortaleza', 'França', 'Freire', 'Freitas', 'Frota', 'Furquim', 'Furtado', 'Galvão', 'Gama', 'Garrastazu', 'Gomes', 'Gonçales', 'Gonçalves', 'Gonzaga', 'Gouveia', 'Guimarães', 'Gusmão', 'Henriques', 'Hernandes', 'Holanda', 'Homem', 'Hora', 'Hungria', 'Jardim', 'Junqueira', 'Lacerda', 'Lange', 'Leite', 'Leme', 'Lins', 'Locatelli', 'Lopes', 'Luz', 'Macedo', 'Machado', 'Madureira', 'Maduro', 'Magalhães', 'Mairinque', 'Malafaia', 'Malta', 'Mariz', 'Marques', 'Martins', 'Massa', 'Matos', 'Médici', 'Meireles', 'Mello', 'Melo', 'Mendes', 'Mendonça', 'Menino', 'Mesquita', 'Miranda', 'Moraes', 'Morais', 'Morato', 'Moreira', 'Moro', 'Monteiro', 'Muniz', 'Nantes', 'Nascimento', 'Navarro', 'Naves', 'Negreiros', 'Negrete', 'Neves', 'Nóbrega', 'Nogueira', 'Noronha', 'Nunes', 'Oliva', 'Oliveira', 'Outeiro', 'Pacheco', 'Padrão', 'Paes', 'Pais', 'Paiva', 'Paixão', 'Papanicolau', 'Parga', 'Pascal', 'Pascoal', 'Pasquim', 'Patriota', 'Peçanha', 'Pedrosa', 'Pedroso', 'Peixoto', 'Pensamento', 'Penteado', 'Pereira', 'Peres', 'Pessoa', 'Pestana', 'Pimenta', 'Pimentel', 'Pinheiro', 'Pires', 'Poeta', 'Policarpo', 'Porto', 'Portugal', 'Prado', 'Prudente', 'Quaresma', 'Queirós', 'Queiroz', 'Ramalhete', 'Ramalho', 'Ramires', 'Ramos', 'Rangel', 'Reis', 'Resende', 'Ribeiro', 'Rios', 'Rodrigues', 'Roma', 'Romão', 'Sá', 'Sacramento', 'Sampaio', 'Sampaulo', 'Sampedro', 'Sanches', 'Santacruz', 'Santana', 'Santander', 'Santarrosa', 'Santiago', 'Santos', 'Saragoça', 'Saraiva', 'Saramago', 'Seixas', 'Serra', 'Serrano', 'Silva', 'Silveira', 'Simões', 'Siqueira', 'Soares', 'Soeiro', 'Sousa', 'Souza', 'Tavares', 'Teixeira', 'Teles', 'Torquato', 'Trindade', 'Uchoa', 'Uribe', 'Ustra', 'Valadares', 'Valença', 'Valente', 'Varela', 'Vasconcelos', 'Vasques', 'Vaz', 'Veiga', 'Velasques', 'Veloso', 'Viana', 'Vieira', 'Vilela', 'Vilhena', 'Xavier', 'Zampol'
         ];
 
-        $nome = array_rand($nomes);
+        $nome = $nomes[array_rand($nomes)];
         if ($comSobrenome) {
-            $nome .= ' ' . array_rand($sobrenomes);
+            $nome .= ' ' . $sobrenomes[array_rand($sobrenomes)];
         }
 
         return $nome;
@@ -40,22 +40,23 @@ class GeneratorsUtils
     public static function genDtNascimento(?int $idadeMinima = 0, ?int $idadeMaxima = 120)
     {
         $esteAno = (new \DateTime())->format('Y');
-        $dia = [
-            1 => rand(1, 31),
-            2 => rand(1, 28),
-            3 => rand(1, 31),
-            4 => rand(1, 30),
-            5 => rand(1, 31),
-            6 => rand(1, 30),
-            7 => rand(1, 31),
-            8 => rand(1, 31),
-            9 => rand(1, 30),
-            10 => rand(1, 31),
-            11 => rand(1, 30),
-            12 => rand(1, 31),
+        $dias = [
+            '01' => rand(1, 31),
+            '02' => rand(1, 28),
+            '03' => rand(1, 31),
+            '04' => rand(1, 30),
+            '05' => rand(1, 31),
+            '06' => rand(1, 30),
+            '07' => rand(1, 31),
+            '08' => rand(1, 31),
+            '09' => rand(1, 30),
+            '10' => rand(1, 31),
+            '11' => rand(1, 30),
+            '12' => rand(1, 31),
         ];
-        $mes = rand(1, 12);
-        return DateTimeUtils::parseDateStr($esteAno - rand($idadeMinima, $idadeMaxima), $mes, $dia[$mes]);
+        $mes = str_pad(rand(1, 12),2,'0',STR_PAD_LEFT);
+        $dia = str_pad($dias[$mes],2,'0',STR_PAD_LEFT);
+        return DateTimeUtils::parseDateStr($esteAno - rand($idadeMinima, $idadeMaxima) . '-' . $mes . '-' . $dia);
     }
 
 
@@ -66,7 +67,8 @@ class GeneratorsUtils
     public static function genTelefone(?int $ddd = null, ?bool $celular = true, ?bool $formatado = true)
     {
         $ddds = [61, 62, 64, 65, 66, 67, 82, 71, 73, 74, 75, 77, 85, 88, 98, 99, 83, 81, 87, 86, 89, 84, 79, 68, 96, 92, 97, 91, 93, 94, 69, 95, 63, 27, 28, 31, 32, 33, 34, 35, 37, 38, 21, 22, 24, 11, 12, 13, 14, 15, 16, 17, 18, 19, 41, 42, 43, 44, 45, 46, 51, 53, 54, 55, 47, 48, 49];
-        $telefone = ($ddd ? $ddd : array_rand($ddds)) . ($celular ? rand(80, 99) : 3) . str_pad(rand(0, 99999999), 8, '0', STR_PAD_LEFT);
+        $ddd = $ddd ?? $ddds[array_rand($ddds)];
+        $telefone = $ddd . ($celular ? rand(80, 99) : 3) . str_pad(rand(0, 9999999), 7, '0', STR_PAD_LEFT);
         return $formatado ? StringUtils::formataTelefone($telefone) : $telefone;
     }
 
