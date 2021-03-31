1e8e1c7321eb9af6dc356255c04138d59a41bd45
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-03-31 18:49:44 -0300

    App e AppConfig com @ApiResource

--- src/Entity/Config/AppConfig.php
+++ src/Entity/Config/AppConfig.php
@@ -2,6 +2,11 @@
 
 namespace CrosierSource\CrosierLibBaseBundle\Entity\Config;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use CrosierSource\CrosierLibBaseBundle\Doctrine\Annotations\EntityHandler;
 use CrosierSource\CrosierLibBaseBundle\Doctrine\Annotations\NotUppercase;
 use CrosierSource\CrosierLibBaseBundle\Entity\EntityId;
 use CrosierSource\CrosierLibBaseBundle\Entity\EntityIdTrait;
@@ -9,6 +14,30 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
+ * @ApiResource(
+ *     normalizationContext={"groups"={"entity","entityId"},"enable_max_depth"=true},
+ *     denormalizationContext={"groups"={"entity"},"enable_max_depth"=true},
+ *
+ *     itemOperations={
+ *          "get"={"path"="/core/config/appConfig/{id}", "security"="is_granted('ROLE_ADMIN')"},
+ *          "put"={"path"="/core/config/appConfig/{id}", "security"="is_granted('ROLE_ADMIN')"},
+ *          "delete"={"path"="/core/config/appConfig/{id}", "security"="is_granted('ROLE_ADMIN')"}
+ *     },
+ *     collectionOperations={
+ *          "get"={"path"="/core/config/appConfig", "security"="is_granted('ROLE_ADMIN')"},
+ *          "post"={"path"="/core/config/appConfig", "security"="is_granted('ROLE_ADMIN')"}
+ *     },
+ *
+ *     attributes={
+ *          "pagination_items_per_page"=10,
+ *          "formats"={"jsonld", "csv"={"text/csv"}}
+ *     }
+ * )
+ *
+ * @ApiFilter(SearchFilter::class, properties={"appUUID": "exact", "chave": "partial"})
+ * @ApiFilter(OrderFilter::class, properties={"chave"}, arguments={"orderParameterName"="order"})
+ *
+ * @EntityHandler(entityHandlerClass="CrosierSource\CrosierLibBaseBundle\EntityHandler\Config\AppConfigEntityHandler")
  *
  * @ORM\Entity(repositoryClass="CrosierSource\CrosierLibBaseBundle\Repository\Config\AppConfigRepository")
  * @ORM\Table(name="cfg_app_config")
