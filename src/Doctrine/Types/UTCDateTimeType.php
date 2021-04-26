f145b9860816687a11d2ef64a0a3b4beb94ed1b5
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2021-04-26 18:30:55 -0300

    src/Doctrine/Types/UTCDateTimeType

---
+++ src/Doctrine/Types/UTCDateTimeType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CrosierSource\CrosierLibBaseBundle\Doctrine\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType;
+
+class UTCDateTimeType extends DateTimeType
+{
+    /** @var \DateTimeZone|null */
+    private static $utc;
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof \DateTime or $value instanceof \DateTimeImmutable) {
+            $value->setTimezone(self::getUtc());
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value || $value instanceof \DateTime) {
+            return $value;
+        }
+
+        $converted = \DateTime::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            self::$utc ? self::$utc : self::$utc = new \DateTimeZone('UTC')
+        );
+
+        if (!$converted) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeFormatString()
+            );
+        }
+
+        return $converted;
+    }
+
+    public static function getUtc(): \DateTimeZone
+    {
+        return self::$utc ? self::$utc : self::$utc = new \DateTimeZone('UTC');
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return true;
+    }
+}
