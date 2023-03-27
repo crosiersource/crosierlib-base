c4fe716896318ee78618edc3f955c1408f05ac76
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-03-27 11:30:52 -0300

    Mais correções nos pacotes

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,9 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     CrosierSource\CrosierLibBaseBundle\CrosierLibBaseBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
