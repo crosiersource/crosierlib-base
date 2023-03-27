cdaee6a73051d9ad0b54348b816ce5f985f238d5
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-03-27 11:37:16 -0300

    Adicionando dependências no require-dev

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
