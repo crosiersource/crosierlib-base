60f76806bb5e072b1f2964a13a3a1efc6b463d52
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2024-03-06 11:34:46 -0300

    PdfUtils::downloadPdf

--- src/Utils/PdfUtils/PdfUtils.php
+++ src/Utils/PdfUtils/PdfUtils.php
@@ -4,6 +4,8 @@ namespace CrosierSource\CrosierLibBaseBundle\Utils\PdfUtils;
 
 use Dompdf\Dompdf;
 use Dompdf\Options;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 /**
  * @author Carlos E Pauluk
@@ -31,6 +33,27 @@ class PdfUtils
         gc_enable();
 
         return $dompdf->output();
+    }
+
+    
+    public static function genDownloadPdf(string $filename, string $html): ?Response
+    {
+        $pdfContent = self::genPdf($html);
+        return self::downloadPdf($filename, $pdfContent);
+    }
+    
+    
+    public static function downloadPdf(string $filename, $pdfContent): ?Response
+    {
+        $response = new Response($pdfContent);
+        $response->headers->set('Content-Type', 'application/pdf');
+
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $filename
+        );
+        $response->headers->set('Content-Disposition', $disposition);
+        return $response;
     }
 
 }
