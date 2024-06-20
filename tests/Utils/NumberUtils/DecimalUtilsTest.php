b9effe40bf391316deb4107046c09733ec2e1647
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2024-06-20 17:28:21 -0300

    Mais testes gerados pelo GPT.

--- tests/Utils/NumberUtils/DecimalUtilsTest.php
+++ tests/Utils/NumberUtils/DecimalUtilsTest.php
@@ -81,13 +81,79 @@ class DecimalUtilsTest extends TestCase
             [0.02, [0, 1, 0], [0.00, 0.02, 0.00]],
             [0.02, [50, 50, 0], [0.01, 0.01, 0.00]],
             [151.15, [0.0, 990.0], [0.0, 151.15]],
+
+            // Novos casos de teste
+            // 1. Valor a ser dividido proporcionalmente entre parcelas com valores iguais
+            [100, [50, 50, 50, 50], [25, 25, 25, 25]],
+
+            // 2. Valor a ser dividido proporcionalmente entre parcelas com valores diferentes
+            [100, [10, 20, 30, 40], [10, 20, 30, 40]],
+
+            // 3. Valor a ser dividido proporcionalmente com a diferença na última parcela (resto pequeno)
+            [99.99, [33, 33, 33], [33.33, 33.33, 33.33]],
+
+            // 4. Valor a ser dividido proporcionalmente com a diferença na primeira parcela (resto pequeno)
+            [99.99, [10, 20, 30, 40], [10, 20, 30, 39.99]],
+
+            // 5. Valor a ser dividido entre parcelas, incluindo zero
+            [100, [0, 50, 50], [0.00, 50.00, 50.00]],
+
+            // 6. Valor a ser dividido proporcionalmente entre parcelas com valores negativos e positivos
+            [100, [-50, 150, 100], [-25.00, 75.00, 50.00]],
+
+            // 7. Valor a ser dividido proporcionalmente quando o valor total é zero
+            [0, [10, 20, 30], [0.00, 0.00, 0.00]],
+
+            // 8. Valor a ser dividido proporcionalmente entre parcelas com um valor muito maior que os outros
+            [100, [1, 1, 998], [0.10, 0.10, 99.80]],
+
+            // 9. Valor a ser dividido proporcionalmente entre parcelas muito pequenas
+            [0.03, [0.01, 0.01, 0.01], [0.01, 0.01, 0.01]],
+
+            // 10. Valor a ser dividido proporcionalmente com diferença decentes entre valores de parcelas
+            [100, [5, 15, 80], [5.00, 15.00, 80.00]],
+
+            // 11. Valor a ser dividido proporcionalmente com parcelas iguais a zero
+            [0.02, [0, 0, 1], [0.00, 0.00, 0.02]],
+
+            // 12. Valor a ser dividido proporcionalmente com valores e diferença na primeira parcela
+            [250, [40, 60, 100], [50, 75, 125]],
+
+            // 13. Valor a ser dividido proporcionalmente com uma parcela significativamente maior
+            [100, [1, 1, 98], [1, 1, 98]],
+
+            // 14. Valor a ser dividido proporcionalmente com grandes valores nas parcelas
+            [1000, [100, 200, 700], [100, 200, 700]],
+
+            // 15. Valor a ser dividido proporcionalmente com valor muito pequeno
+            [0.04, [1, 2, 1], [0.01, 0.02, 0.01]],
+
+            // 16. Valor a ser dividido proporcionalmente com um único valor grande na parcela
+            [500, [1, 999], [0.5, 499.5]],
+
+            // 17. Valor a ser dividido proporcionalmente com uma parcela zero e o resto positivo
+            [100, [0, 100], [0, 100]],
+
+            // 18. Valor a ser dividido proporcionalmente com valor zero a ser distribuído
+            [0, [50, 100], [0.00, 0.00]],
+
+            // 19. Valor a ser dividido proporcionalmente com valores grandes em múltiplas parcelas
+            [1000, [100, 200, 300, 400], [100, 200, 300, 400]],
+
+            // 20. Valor a ser dividido proporcionalmente com valores muito pequenos e diferença na última parcela
+            [0.05, [0.01, 0.02, 0.02], [0.01, 0.02, 0.02]],
         ];
+                
+        
         foreach ($testes as $t) {
             $this->assertEquals($t[2], DecimalUtils::dividirValorProporcionalmente($t[0], $t[1], false));
             $somaPartes = DecimalUtils::somarValoresMonetarios($t[2]);
             $this->assertEquals($t[0], $somaPartes);
         }
     }
+    
+    
+    
 
 
 }
